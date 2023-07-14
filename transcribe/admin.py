3f83ef9a03371c87fbd3257a19e7b985357a6782
metajs <zycode277@gmail.com>
2023-07-14 16:50:34 +0800

    feature: add basic model

--- transcribe/admin.py
+++ transcribe/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
-
+from .models import Transcription
 # Register your models here.
+
+@admin.register(Transcription)
+class TranscriptionAdmin(admin.ModelAdmin):
+    # fields = ["path"]
+    def status(obj):
+        if obj.status == 0:
+            return "Queue"
+        elif obj.status == 1:
+            return "Transcribing"
+        elif obj.status == 2:
+            return "Completed"
+    def transcription_progress(obj):
+        return f"{obj.progress}%"
+    list_display = ['path', status, transcription_progress]
--- transcribe/models.py
+++ transcribe/models.py
@@ -1,5 +1,7 @@
 from django.db import models
-class TranscriptionQueue(models.Model):
+
+# Create your models here.
+class Transcription(models.Model):
         id = models.AutoField(primary_key=True, auto_created=True)
         path = models.CharField(max_length=200)
         created_at = models.DateTimeField(auto_now_add=True)
